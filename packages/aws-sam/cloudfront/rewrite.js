04815e6459a5576277cc707b83b622001b6cd920
Paul Kreichauf <paul.kreichauf@gmx.de>
2019-01-03 22:16:13 +0100

    add setup CDN to aws-sam and change space bucket

--- packages/aws-sam/cloudfront/rewrite.js
+++ packages/aws-sam/cloudfront/rewrite.js
@@ -2,15 +2,16 @@
 //
 exports.handler = (event, context, callback) => {
 	// Get request from CloudFront event
-	var request = event.Records[0].cf.request;
+	const { request } = event.Records[0].cf;
 	// Extract the URI from the request
-	var requestUrl = request.uri;
+	const requestUriWithoutHash = request.uri.split("#")[0];
+	const requestUrlPath = requestUriWithoutHash.split("?")[0];
+	const lastPathPart = requestUrlPath.split("/").pop();
 	//
-	if (requestUrl.indexOf(".") === -1) {
+	if (lastPathPart.indexOf(".") === -1) {
 		// Replace the received URI with the URI that includes the index page
 		request.uri = "/index.html";
 	}
-
 	// Return to CloudFront
 	return callback(null, request);
 };
--- packages/aws-sam/cloudfront/rewrite.spec.js
+++ packages/aws-sam/cloudfront/rewrite.spec.js
@@ -1,4 +1,4 @@
-const { rewrite } = require("./rewrite");
+const { handler } = require("./rewrite");
 
 const createEvent = uri => ({
 	Records: [
@@ -15,53 +15,87 @@ const createEvent = uri => ({
 	]
 });
 
-describe("sam rewrite should append index.html on uris ending with /", () => {
-	test("/ uri", () => {
-		const callback = jest.fn();
-		const event = createEvent("/");
-		//
-		rewrite(event, null, callback);
-		//
-		expect(callback.mock.calls.length).toBe(1);
-		expect(callback.mock.calls[0][0]).toBeNull();
-		expect(callback.mock.calls[0][1]).toEqual(
-			createEvent("/index.html").Records[0].cf.request
-		);
-	});
-	test("/blog uri", () => {
-		const callback = jest.fn();
-		const event = createEvent("/blog");
-		//
-		rewrite(event, null, callback);
-		//
-		expect(callback.mock.calls.length).toBe(1);
-		expect(callback.mock.calls[0][0]).toBeNull();
-		expect(callback.mock.calls[0][1]).toEqual(
-			createEvent("/blog").Records[0].cf.request
-		);
-	});
-	test("/blog/ uri", () => {
-		const callback = jest.fn();
-		const event = createEvent("/blog/");
-		//
-		rewrite(event, null, callback);
-		//
-		expect(callback.mock.calls.length).toBe(1);
-		expect(callback.mock.calls[0][0]).toBeNull();
-		expect(callback.mock.calls[0][1]).toEqual(
-			createEvent("/blog/index.html").Records[0].cf.request
-		);
-	});
-	test("/test.jpg uri", () => {
-		const callback = jest.fn();
-		const event = createEvent("/test.jpg");
-		//
-		rewrite(event, null, callback);
-		//
-		expect(callback.mock.calls.length).toBe(1);
-		expect(callback.mock.calls[0][0]).toBeNull();
-		expect(callback.mock.calls[0][1]).toEqual(
-			createEvent("/test.jpg").Records[0].cf.request
-		);
-	});
+test("/ root redirect to index.html", () => {
+	const callback = jest.fn();
+	const event = createEvent("/");
+	//
+	handler(event, null, callback);
+	//
+	expect(callback.mock.calls.length).toBe(1);
+	expect(callback.mock.calls[0][0]).toBeNull();
+	expect(callback.mock.calls[0][1]).toEqual(
+		createEvent("/index.html").Records[0].cf.request
+	);
+});
+test("/blog redirect to index.html", () => {
+	const callback = jest.fn();
+	const event = createEvent("/blog");
+	//
+	handler(event, null, callback);
+	//
+	expect(callback.mock.calls.length).toBe(1);
+	expect(callback.mock.calls[0][0]).toBeNull();
+	expect(callback.mock.calls[0][1]).toEqual(
+		createEvent("/index.html").Records[0].cf.request
+	);
+});
+test("ending with slash redirect to index.html", () => {
+	const callback = jest.fn();
+	const event = createEvent("/blog/");
+	//
+	handler(event, null, callback);
+	//
+	expect(callback.mock.calls.length).toBe(1);
+	expect(callback.mock.calls[0][0]).toBeNull();
+	expect(callback.mock.calls[0][1]).toEqual(
+		createEvent("/index.html").Records[0].cf.request
+	);
+});
+test("it ignores get parameter", () => {
+	const callback = jest.fn();
+	const event = createEvent("/blog/?asd=a.a");
+	//
+	handler(event, null, callback);
+	//
+	expect(callback.mock.calls.length).toBe(1);
+	expect(callback.mock.calls[0][0]).toBeNull();
+	expect(callback.mock.calls[0][1]).toEqual(
+		createEvent("/index.html").Records[0].cf.request
+	);
+});
+test("it cares only about the last part", () => {
+	const callback = jest.fn();
+	const event = createEvent("/aaa.bbb/blog/");
+	//
+	handler(event, null, callback);
+	//
+	expect(callback.mock.calls.length).toBe(1);
+	expect(callback.mock.calls[0][0]).toBeNull();
+	expect(callback.mock.calls[0][1]).toEqual(
+		createEvent("/index.html").Records[0].cf.request
+	);
+});
+test("it ignores hashes", () => {
+	const callback = jest.fn();
+	const event = createEvent("/blog#aaa.bbb");
+	//
+	handler(event, null, callback);
+	//
+	expect(callback.mock.calls.length).toBe(1);
+	expect(callback.mock.calls[0][0]).toBeNull();
+	expect(callback.mock.calls[0][1]).toEqual(
+		createEvent("/index.html").Records[0].cf.request
+	);
+});
+test("/test.jpg uri", () => {
+	const callback = jest.fn();
+	const event = createEvent("/test.jpg");
+	//
+	handler(event, null, callback);
+	//
+	expect(callback.mock.calls.length).toBe(1);
+	expect(callback.mock.calls[0][0]).toBeNull();
+	expect(callback.mock.calls[0][1]).toEqual(
+		createEvent("/test.jpg").Records[0].cf.request
+	);
 });
